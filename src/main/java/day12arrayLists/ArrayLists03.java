41879fd02687507830a2f21acc1c0482a95c61b9
yasin mesekoparan <y.mesekoparan1510@gmail.com>
2023-08-25 00:50:22 +0300

    yasel

---
+++ src/main/java/day12arrayLists/ArrayLists03.java
@@ -0,0 +1,46 @@
+package day12arrayLists;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayLists03 {
+
+    public static void main(String[] args) {
+
+        //Example : Bir tane Integer List olusturunuz
+        //           Bu List'te birbirine en yakin iki tamsayiyi yaziniz
+        //           [12, 23, 10, 19] ==> 12 and 10
+
+        // ADVANCE SORU,İNTERWİEW DE ÇİKMİS,İYİ ÇALİS..
+
+
+        List<Integer> nums = new ArrayList<>();
+        nums.add(12);
+        nums.add(23);
+        nums.add(10);
+        nums.add(19);
+
+        Collections.sort(nums); //sort methodu list'lerde Collections la kullanilir
+        System.out.println(nums);
+
+        int minDiff=nums.get(1)- nums.get(0);
+
+        for (int i=1; i<nums.size(); i++) {
+            minDiff=Math.min(minDiff,(nums.get(i)-nums.get(i-1)));
+
+        }
+        System.out.println(minDiff);
+
+        //en kucuk farki bulduk,bu minDiffi hangi iki sayidan elde ettigimizi buluyoruz
+
+        for (int i = 1; i <nums.size() ; i++) {
+            if (nums.get(i)-nums.get(i-1)==minDiff){
+                System.out.println(nums.get(i) +" ve "+ nums.get(i-1));
+            }
+
+        }
+
+    }
+}
