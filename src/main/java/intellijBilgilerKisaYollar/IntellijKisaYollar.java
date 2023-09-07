c34fa20999b497552adc5d7731e7b2f03738e7a3
yasin mesekoparan <y.mesekoparan@gmail.com>
2023-09-08 00:27:57 +0300

    08.09

--- src/main/java/intellijBilgilerKisaYollar/IntellijKisaYollar.java
+++ src/main/java/intellijBilgilerKisaYollar/IntellijKisaYollar.java
@@ -4,12 +4,25 @@ public class IntellijKisaYollar {
 
 
     /*
-     Cntrl+slash  //....
-     Cntr+shift+slash /* ***... YORUM İCİN SECTİGİM RENGİN KODU "DBABAB" bu da mavi"0863D0"
-     Cntrl+shift+A find action..bir seyin ustune cursoru birakip find action basarsak yukarda arama
-     ekrani açilir,renklerle alakali birsey yapacaksak jump to colors and fonts secenegine gireriz
-
+     Cntrl+slash == //....
+     Cntr+shift+slash== /* ***... YORUM İCİN SECTİGİM RENGİN KODU "DBABAB" bu da mavi"0863D0"
+     Cntrl+shift+A ==find action..bir seyin ustune cursoru birakip find action basarsak yukarda arama
+         ekrani açilir,renklerle alakali birsey yapacaksak jump to colors and fonts secenegine gireriz
+     Alt+fare(sol tuş)basılı tutularak asagi sürüklenirse==;cursor bütün satirlarda ayni hizada yanip
+         sonmeye başlar..hepsinde ayni hizada ekleme ya da silme yapilabilir.
+     cntrl+alt+l ===tüm sayfayi düzene sokar
+     Alt+j == bir kelimenin tamamini farenin sol tusuna basili tutarak seçip,alt+j ye basildignda
+        ,her basışımızda bir sonrakini almak üzere aşagıya doğru tüm ayni kelimeleri seçer,istersek
+         degistirebiliriz,hepsini..mesela bir variable ismini degiştirecegiz,en üsttekini seçip,
+         alt+j ye her basışımızda aşagiya doğru hepsini secip baska bir isimle degistirebiliriz
      */
+     */
+     */
+     */
+     */
+
+
+
 
 
 
